3bbe5cad813232d39fff2b894f031c7a6c45c093
muhta <C503758@newcastle.ac.uk>
2026-03-02 16:06:35 +0000

    Added button layout on calculator & added more comments.

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-##Creating a window
+# Creating a window
 
 window = tk.Tk() ##Creates the calculator window
 window.title("Calculator")
@@ -12,13 +12,17 @@ entry = tk.Entry(window, width=16, font=("Arial", 20), borderwidth=5, relief="ri
 entry.grid(row=0, column=0, padx=10, columnspan=4)
 
 ##Button functions
+
+#Function when button is pressed
 def click(num):
     entry.delete(0, tk.END)
-    entry.insert(0, num)
+    entry.insert(0, num) #Adds number/operator to the screen
 
+#Clears the display on the calculator when C button is pressed
 def clear():
     entry.delete(0, tk.END)
 
+#Runs when = button is pressed
 def equal():
     try:
         answer = eval(entry.get())
@@ -27,6 +31,17 @@ def equal():
     except:
         entry.delete(0, tk.END)
         entry.insert(0, "Error")
+
+## Button layout on calculator
+
+buttons = [
+    ('7', 1,0), ('8',1,1), ('9',1,2), ('/',1,3),
+    ('4',2,0), ('5',2,1), ('6',2,2), ('*',2,3),
+    ('1',3,0), ('2',3,1), ('3',3,2), ('-',3,3),
+    ('0',4,0), ('.',4,1), ('=',4,2), ('+',4,3),
+]
+
+
 
 
 
