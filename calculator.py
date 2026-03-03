29e8105b5af9058c59fe529411ac938b779518f0
muhta <C503758@newcastle.ac.uk>
2026-03-03 14:02:46 +0000

    Added comments for clarity.

--- calculator.py
+++ calculator.py
@@ -3,32 +3,36 @@ import tkinter as tk
 # Creating a window
 
 window = tk.Tk() ##Creates the calculator window
-window.title("Calculator")
+window.title("Calculator") ##Sets the text on top of the screen
 window.geometry("300x400")
 
 ## Display screen
 ##Creating the calculator screen
-entry = tk.Entry(window, width=16, font=("Arial", 20), borderwidth=5, relief="ridge")
-entry.grid(row=0, column=0, padx=10, columnspan=4)
+entry = tk.Entry(window, width=16, font=("Arial", 20), borderwidth=5, relief="ridge") ## Puts it inside the window
+entry.grid(row=0, column=0, padx=10, columnspan=4) ## Places the screen at row 0 and stretches across 4 columns
 
 ##Button functions
 
 #Function when button is pressed
 def click(num):
+    ##Insert number/operator at the end of the current text
     entry.delete(0, tk.END)
     entry.insert(0, num) #Adds number/operator to the screen
 
 #Clears the display on the calculator when C button is pressed
 def clear():
-    entry.delete(0, tk.END)
+    entry.delete(0, tk.END) ## Deletes everything on the calculator from start to end
 
 #Runs when = button is pressed
 def equal():
     try:
+        ##Gets current expression from display & evaluates the mathematical expression
         answer = eval(entry.get())
+        ## Clears old expression and insert the result
         entry.delete(0, tk.END)
         entry.insert(0, answer)
     except:
+        ##If there is an invalid input then display is cleared and displays error
         entry.delete(0, tk.END)
         entry.insert(0, "Error")
 
@@ -42,6 +46,7 @@ buttons = [
 ]
 
 #Creates buttons
+## Loop through button list and create each button
 for (text,row,col) in buttons:
     if text =="=":
         tk.Button(window, text=text, width = 5, height=2, command = equal).grid(row=row, column=col)
@@ -49,9 +54,9 @@ for (text,row,col) in buttons:
         tk.Button(window, text=text, width =5, height = 2, command = lambda t =text: click(t)).grid(row=row, column=col)
 
 
-#Clear button
+#Clear button that expands across all columns
 tk.Button(window, text = "C", width = 22, height = 2, command = clear).grid(row = 5, column = 0, columnspan = 4)
-window.mainloop()
+window.mainloop() ##Keeps window open
 
 
 
